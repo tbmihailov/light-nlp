26a2e965eb526a3c8c17e1fa841762adc8200904
Todor Mihaylov <tbmihailov@gmail.com>
2016-02-26 15:37:07 +0200

    Added LightNlpWebApi.Client for rest request to LightNlp Web Api

---
+++ LightNlp/LightNlpWebApi.Client/LightNlpWebApiRestClient.cs
@@ -0,0 +1,97 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightNlpWebApi.Client
+{
+    public class LightNlpWebApiRestClient
+
+    {
+        public const string AUTH_TOKEN = "AUTH_TOKEN";
+
+        string _AuthKey;
+
+        string _apiUrl;
+
+        public LightNlpWebApiRestClient(string apiUrl)
+            : this(apiUrl, string.Empty)
+        { }
+
+
+        public LightNlpWebApiRestClient(string apiUrl, string agentAuthKey)
+        {
+            _apiUrl = apiUrl;
+            _AuthKey = agentAuthKey;
+        }
+
+        private void AddCredentialsToRestRequest(RestRequest request)
+        {
+            if (!string.IsNullOrEmpty(_AuthKey))
+            {
+                request.AddHeader(AUTH_TOKEN, _AuthKey);
+            }
+        }
+
+        #region CategorizeTextDoc
+        public void CategorizeTextDocAsync(Dictionary<string, string> activity, Action<IRestResponse<Dictionary<string, string>>> callback)
+        {
+            RestClient client = new RestClient(_apiUrl);
+            var request = PrepareCategorizeTextDocRequest(activity);
+
+            client.ExecuteAsync<Dictionary<string, string>>(request, callback);
+        }
+
+        public IRestResponse<Dictionary<string, string>> CategorizeTextDoc(Dictionary<string, string> activity)
+        {
+            RestClient client = new RestClient(_apiUrl);
+            var request = PrepareCategorizeTextDocRequest(activity);
+
+            return client.Execute<Dictionary<string, string>>(request);
+        }
+
+        private RestRequest PrepareCategorizeTextDocRequest(Dictionary<string, string> activity)
+        {
+            var request = new RestRequest(new Uri("/api/Services/CategorizeTextDoc", UriKind.Relative), Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(activity);
+            AddCredentialsToRestRequest(request);
+
+            return request;
+        }
+        #endregion
+
+        #region CategorizeTextDoc
+        public void CategorizeTextDocMultiAsync(List<Dictionary<string, string>> activity, Action<IRestResponse<List<Dictionary<string, string>>>> callback)
+        {
+            RestClient client = new RestClient(_apiUrl);
+            var request = PrepareCategorizeTextDocMultiRequest(activity);
+
+            client.ExecuteAsync<List<Dictionary<string, string>>>(request, callback);
+        }
+
+        public IRestResponse<List<Dictionary<string, string>>> CategorizeTextDocMulti(List<Dictionary<string, string>> docs)
+        {
+            RestClient client = new RestClient(_apiUrl);
+            var request = PrepareCategorizeTextDocMultiRequest(docs);
+
+            return client.Execute<List<Dictionary<string, string>>>(request);
+        }
+
+        private RestRequest PrepareCategorizeTextDocMultiRequest(List<Dictionary<string, string>> activity)
+        {
+            var request = new RestRequest(new Uri("/api/Services/CategorizeTextDocMulti", UriKind.Relative), Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(activity);
+            AddCredentialsToRestRequest(request);
+
+            return request;
+        }
+
+        #endregion
+    }
+}
---
+++ LightNlp/LightNlpWebApi.Client/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace LightNlpWebApi.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string authToken = "<token if authorization is enabled>";
+            string serviceUrl = "http://localhost:8080";
+            LightNlpWebApiRestClient client = new LightNlpWebApiRestClient(serviceUrl);
+
+            //Single doc categorization
+            Dictionary<string, string> textDoc = new Dictionary<string, string>()
+            {
+                { "Id", "1" },
+                { "Text", "Има огромна дупка на улицата!!!" }
+            };
+
+            var result = client.CategorizeTextDoc(textDoc);
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var categorizedDoc = result.Data;
+                Console.WriteLine("{0} - {1}", categorizedDoc["Label"], categorizedDoc["LabelName"]);
+            }
+
+            //multi docs
+            List<Dictionary<string, string>> textDocs = new List<Dictionary<string, string>>()
+            { new Dictionary<string, string>()
+            {
+                { "Id", "1" },
+                { "Text", "Има огромна дупка на улицата!!!" }
+            },
+            new Dictionary<string, string>()
+            {
+                { "Id", "2" },
+                { "Text", "Счупена люлка на детска площадка!!!" }
+            },
+            };
+
+            var resultList = client.CategorizeTextDocMulti(textDocs);
+            if (resultList.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var categorizedDocs = resultList.Data;
+                foreach (var catDoc in categorizedDocs)
+                {
+                    Console.WriteLine("[{2}]{0} - {1}", catDoc["Label"], catDoc["LabelName"], catDoc["Id"]);
+                }
+
+            }
+        }
+    }
+}
+
